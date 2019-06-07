83ab2d0baa9bbd20ffdf482bf6dd42d95d46a94a
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-06-07 15:37:19 +0200

    change model params

--- sr_model.py
+++ sr_model.py
@@ -25,9 +25,9 @@ class Upconv(nn.Module):
 class SRCNN(nn.Module):
     def __init__(self):
         super(SRCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, stride=1, padding=0)
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, stride=1, padding=0)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
