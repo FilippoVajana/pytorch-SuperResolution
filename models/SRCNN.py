4916a4cc3a7ba1b0d3b74859d75b38dd17e27a96
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-07-03 12:55:29 +0200

    new models package

---
+++ models/SRCNN.py
@@ -0,0 +1,14 @@
+from sr_imports import *
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        return x
