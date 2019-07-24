2048311b34c581523510ad7b5e0a8f30f79836be
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-07-24 16:47:23 +0200

    add benchmark script

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,8 @@
 from sr_imports import *
-import sr_utils as util
+from models import SRCNN
+from engine import model_trainer
+
+from utilities.utils import create_folder
 import data.dataset as data
 import jsonpickle
 
@@ -7,25 +10,46 @@ class BenchmarkConfig(object):
     JSON_NAME = "runconfig.json"
 
     def __init__(self):
-        # Data
-        self.train_data = ""
-        self.validation_data = ""
-        self.test_data = ""
+        # options
+        self.device = 'cpu'
 
-        # Train loop        
+        # data
+        self.use_small_dataset = False
+        self.train_data = ''
+        self.validation_data = ''
+        self.test_data = ''
+
+        # train options
+        self.load_model = False    
         self.batch_size = 0
         self.epochs = 0
 
-        # Result
+        # result options
         self.save_output = False
     
     def save(self, destination):
+        """
+        Saves the current configuration inside a JSON file.        
+        Args:
+        --------
+            destination (string): The destination path.        
+        Returns:
+        --------
+            string: The absolute path of the json file.
+        """
         path = os.path.join(destination, BenchmarkConfig.JSON_NAME)
         with open(path, "w") as f:
             json_obj = jsonpickle.encode(self)
             f.write(json_obj)
         return path
 
+    @staticmethod
+    def save_empty(destination):
+        cfg = BenchmarkConfig()
+        cfg_path = cfg.save(destination)
+        return cfg_path
+
+    @staticmethod
     def load(path):
         with open(path, "r") as f:
             json_str = f.read()
@@ -34,56 +58,81 @@ class BenchmarkConfig(object):
 
 
 class Benchmark():
-    def __init__(self, config_path=None):
+    def __init__(self, configuration=None):
+        # set logging
+        logging.basicConfig(level=logging.DEBUG)     
+        
         # set configuration
-        if config_path is None : 
-            print("Initialize empty run configuration")
-            self.configuration = BenchmarkConfig()
+        if configuration is None : 
+            logging.warning("Running with default configuration.")
+            self.cfg = BenchmarkConfig()
         else: 
-            self.configuration = BenchmarkConfig.load(config_path)        
+            self.cfg = configuration 
         
     def __get_id(self):
         # get run id as a time string
         import datetime as dt
         time = dt.datetime.now()
         id = time.strftime("%H_%M_%d_%m") # Hour_Minute_Day_Month
-        return id        
-
-
+        return id       
+    
     def run(self):
         root = os.getcwd()
-        rf_path = root
 
         # get run id
+        logging.info("Get run ID.")
         run_id = self.__get_id()
 
-        # init run folders
-        run_dir = util.create_folder(root, run_id)        
-        tmp_dir = util.create_folder(run_dir, "tmp")
+        # init run folders     
+        logging.info("Creating run directory.")   
+        run_dir = create_folder(root, run_id)
 
         # prepare data
         # 1. preprocessing --> list of functions
         # 2. init datasets
-        train_ds = data.SRDataset(self.configuration.train_data)
-        validation_ds = data.SRDataset(self.configuration.validation_data)
-        test_ds = data.SRDataset(self.configuration.test_data)
+        # TODO: check small-dataset opt
+        logging.info("Creating train set.")
+        train_ds = data.SRDataset(self.cfg.train_data)
+
+        # TODO: check and create
+        logging.info("Creating validation set.")
+        validation_ds = data.SRDataset(self.cfg.validation_data)
+
+        # logging.info("Creating test set.")
+        # test_ds = data.SRDataset(self.cfg.test_data)
 
         # 3. init dataloaders
-        train_dl = tdata.DataLoader(train_ds, self.configuration.batch_size, shuffle=True)
-        validation_dl = tdata.DataLoader(validation_ds, self.configuration.batch_size, shuffle=False)
-        test_dl = tdata.DataLoader(test_ds, batch_size=1, shuffle=False)
+        logging.info("Creating train dataloader.")
+        train_dl = tdata.DataLoader(train_ds, self.cfg.batch_size, shuffle=True)
+
+        logging.info("Creating validation dataloader.")
+        validation_dl = tdata.DataLoader(validation_ds, self.cfg.batch_size, shuffle=False)
+
+        # logging.info("Creating test dataloader.")
+        # test_dl = tdata.DataLoader(test_ds, batch_size=1, shuffle=False)
         
         # init model runner
         # 1. metrics callbacks
         # 2. execution observer
+        
 
         # run model
-        # 1. train + validation
+        # 1. train + validation        
         # 2. test
+        logging.info("Initializing SRCNN model.")
+        model = SRCNN.SRCNN()
+        logging.info("Initializing model trainer.")
+        trainer = model_trainer.Trainer(model, self.cfg.device)
+
+        logging.info("Starting train phase.")
+        trainer.run(self.cfg.epochs, train_dl, validation_dl)
 
         # get results (save + visualize)
-
+        logging.debug(trainer.train_log)
+        logging.debug(trainer.validation_log)
+        
         # clean tmp data
+
 
 if __name__ == "__main__":
     # mock config
