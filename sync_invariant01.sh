f17b1afe32094583b04383f52c2872a38f334859
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-07-24 21:57:25 +0200

    remove sync script

--- sync_invariant01.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-echo "Sync project with Rsync"
-
-rsync -rvh --progress -e'ssh -i /mnt/c/Users/filip/.ssh/FilippoVajanaOrobix_rsa' '/mnt/c/Users/filip/Documents/Projects/SuperResolution' filippo_vajana@192.168.4.169:/home/filippo_vajana/Projects/
-
-# -e"ssh -i /mnt/c/Users/filip/.ssh/FilippoVajanaOrobix_rsa"
