15b0cf9bca482447a490474f09b00e2c733f1415
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-07-30 14:49:43 +0200

    add tester

--- engine/model_tester.py
+++ engine/model_tester.py
@@ -1,4 +1,44 @@
 from imports import *
+from engine.logger import Logger
+from data.metrics import Metrics
+from tqdm import tqdm
+
 
 class Tester():
-    pass
+    def __init__(self, model):
+        self.model = model.to("cpu")
+        self.loss_fn = torch.nn.MSELoss()
+        self.quality = Metrics()
+
+        self.log = Logger()
+        self.log.add_metric("test_loss")
+        self.log.add_metric("psnr")
+
+    def test(self, test_dataloader = None):
+        """
+        Tests the trained model.
+        
+        Keyword Arguments:
+            test_dataloader {DataLoader} -- Test data (default: {None})
+        
+        Raises:
+            Exception: Raises exception if the dataloader is None
+        """
+
+        if test_dataloader == None :
+            raise Exception("Invalid test dataloader.")
+
+        with torch.no_grad():
+            for data in tqdm(test_dataloader):
+                examples, targets = data
+
+                # predict
+                outputs = self.model(examples)
+
+                # compute loss
+                loss = self.loss_fn(outputs, targets)
+                self.log.add_value("test_loss", loss)
+                
+                # compute psnr
+                for e, l in zip(outputs, targets):
+                    self.log.add_value("psnr", self.quality.psnr(e, l))
