64ee00a4619c02408f3368e8019fd2aedb2b241b
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-07-30 17:25:03 +0200

    add helper functions to handle result images

---
+++ benchmarks/utils.py
@@ -0,0 +1,88 @@
+from imports import *
+from data.metrics import Metrics
+import torchvision as torchvision
+
+
+def create_result_img(original, output, target):
+    cols = 3
+    rows = 1
+    toPil = torchvision.transforms.ToPILImage()
+
+    fig = plt.figure()
+    fig.set_size_inches(30, 10)    
+
+    ax1 = fig.add_subplot(rows, cols, 1)
+    ax1.set_title(f"Train image {original.shape}")
+    plt.imshow(toPil(original))
+
+    ax1 = fig.add_subplot(rows, cols, 2)    
+    ax1.set_title(f"Model output (PSNR={Metrics().psnr(output, target).item()})")
+    plt.imshow(toPil(output))
+
+    ax1 = fig.add_subplot(rows, cols, 3)
+    ax1.set_title(f"Target image {target.shape}")
+    plt.imshow(toPil(target))
+
+    return fig
+
+
+def save_result_img(fig, benchmark_dir, idx):
+    fig.savefig(os.path.join(benchmark_dir, f"output_img{idx}"), dpi=250)
+
+
+def display_result_img(fig):
+    fig.show()
+
+
+def collect_result_imgs(model, test_dataloader = None, sample_count = 5, display = False, save = True, save_path = "./"):
+    """
+    Collects test results.
+    
+    Arguments:
+        model {[type]} -- [description]
+    
+    Keyword Arguments:
+        test_dataloader {DataLoader} -- Test data (default: {None})
+        sample_count {int} -- Number of samples (default: {5})
+        display {bool} -- Show result images (default: {False})
+        save {bool} -- Save result images (default: {True})
+        save_path {str} -- Save path (default: {"./"})
+    
+    Raises:
+        Exception: [description]
+    """
+
+    if test_dataloader == None :
+        raise Exception("Invalid test dataloader.")
+
+    results = list()
+
+    model.eval()
+    with torch.no_grad():
+        for idx, data in enumerate(test_dataloader):
+            if idx >= sample_count:
+                break
+
+            example, target = data
+
+            # model output
+            output = model(example)
+
+            # original unscaled image
+            toTensor = torchvision.transforms.ToTensor()
+            original = toTensor(Image.open(test_dataloader.dataset.examples[idx]))
+
+            # create result image
+            logging.info("Creating result image.")
+            output = output.squeeze()
+            target = target.squeeze()
+            res = create_result_img(original, output, target)
+            results.append(res)
+
+            if display:
+                display_result_img(res)
+
+    if save:
+        logging.info(f"Saving {len(results)} result images.")
+        for idx, fig in enumerate(results):
+            save_result_img(fig, save_path, idx)
