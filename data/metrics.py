e21c0f1297ce53e2b252ba304507100a867d87c6
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-08-13 19:24:39 +0200

    remove unused code

--- data/metrics.py
+++ data/metrics.py
@@ -8,15 +8,8 @@ class Metrics():
         mse_3 = F.mse_loss(prediction, target)
         return mse_3
 
-    # @staticmethod
-    # def psnr(target, prediction):
-    #     max_i = torch.Tensor(1).float()
-    #     m = Metrics.mse(target, prediction)
-    #     psnr = 10 * torch.log10(max_i / m)
-    #     return psnr
-
     @staticmethod
     def psnr(target, prediction):
         t = target.data.numpy()
         p = prediction.data.numpy()
-        return compare_psnr(t, p)
+        return compare_psnr(t, p, 1)
