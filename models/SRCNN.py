1eb86672ea4db88f8879d8bceda23e65be043d02
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-08-24 02:13:45 +0200

    new shape for conv layers

--- models/SRCNN.py
+++ models/SRCNN.py
@@ -5,14 +5,16 @@ from torch.nn.modules.upsampling import Upsample
 class SRCNN(nn.Module):
     def __init__(self):
         super(SRCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=4)
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
-        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv4 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
+        self.scaler2x = Upsample(scale_factor=2, mode='bicubic', align_corners=False)
 
-    def forward(self, x):
-        scaler2x = Upsample(scale_factor=2, mode='bicubic', align_corners=False)
-        x = scaler2x(x)
+    def forward(self, x):        
+        x = self.scaler2x(x)
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
         return x
