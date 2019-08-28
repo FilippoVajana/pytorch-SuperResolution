b0064f07c592c239299a358ee3794eec34c22ad0
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-08-28 10:15:01 +0200

    add rgb option in resize

--- init_data.py
+++ init_data.py
@@ -7,7 +7,7 @@ def is_valid(path):
     if not os.path.isdir(path) : return False
     else : return True
 
-def resize(data, destination, size):
+def resize(data, destination, size, rgb):
     """
     Resizes images and saves them as grey scale PIL.
     
@@ -16,15 +16,16 @@ def resize(data, destination, size):
         data {list} -- List of paths
         destination {str} -- Destination folder path
         size {int} -- Final image size
+        rgb {bool} -- If False converts to grey scale
     """
 
     for d in tqdm(data):
-        Image.open(d)\
-            .resize((size, size), Image.BICUBIC)\
-                .convert('L')\
-                    .save(os.path.join(destination, os.path.basename(d)))
+        with Image.open(d) as im:            
+            im = im.resize((size, size), Image.BICUBIC)
+            if rgb == False : im = im.convert('L')        
+            im.save(os.path.join(destination, os.path.basename(d)))
 
-def init_data(source_dir, dest_name, examples_num = 0, img_size = 0, img_mult = 2):
+def init_data(source_dir, dest_name, examples_num = 0, img_size = 0, rgb = False, img_mult = 2):
     # init logger
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger()     
@@ -67,12 +68,12 @@ def init_data(source_dir, dest_name, examples_num = 0, img_size = 0, img_mult = 
     # process examples
     logger.info("Resizing examples.")
     tqdm.write("Resizing examples...")
-    resize(examples[:file_num], dest_ex, img_size)
+    resize(examples[:file_num], dest_ex, img_size, rgb)
 
     # process labels
     logger.info("Resizing labels.")
     tqdm.write("Resizing labels...")
-    resize(labels[:file_num], dest_lab, img_size*img_mult)
+    resize(labels[:file_num], dest_lab, img_size*img_mult, rgb)
 
 
 
@@ -82,6 +83,7 @@ if __name__ == "__main__":
     parser.add_argument('-dl', help='Number of examples, 0=ALL', type=int, action='store', default=0)
     parser.add_argument('-src', help='Data source', type=str, action='store', default='./data/div2k/train/2x_bicubic')
     parser.add_argument('-s', help='Example image size', type=int, action='store', default=32)
+    parser.add_argument('-rgb', help='RGB images', action='store_true', default=False)
     parser.add_argument('-im', help='Label image size multiplier', type=int, action='store', default=2)
     args = parser.parse_args()
 
@@ -89,6 +91,7 @@ if __name__ == "__main__":
     ds_len = args.dl
     ds_src = args.src
     im_size = args.s
+    im_rgb = args.rgb
     im_mult = args.im
 
-    init_data(ds_src, ds_name, ds_len, im_size, im_mult)
+    init_data(ds_src, ds_name, ds_len, im_size, im_rgb, im_mult)
