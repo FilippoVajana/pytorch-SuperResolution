4f91958b6f4939af6f6d94f123dde94077f023dd
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2019-08-29 11:09:21 +0200

    modify srcnn params

--- models/SRCNN.py
+++ models/SRCNN.py
@@ -8,8 +8,8 @@ class SRCNN(nn.Module):
         self.name = f"{self.__class__.__name__}_in{img_channels}"
         self.conv1 = nn.Conv2d(img_channels, 32, kernel_size=9, padding=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=1, padding=0)
-        self.conv3 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
-        self.conv4 = nn.Conv2d(32, img_channels, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=1, padding=0)
+        self.conv4 = nn.Conv2d(64, img_channels, kernel_size=5, padding=2)
         self.scaler2x = Upsample(scale_factor=2, mode='bicubic', align_corners=False)
 
     def forward(self, x):        
